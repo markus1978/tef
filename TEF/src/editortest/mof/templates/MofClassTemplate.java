6d42400be375c8df0873fa551785a063033e2708
Markus Scheidgen <do@not.use>
2006-10-29 16:45:30 +0000

    refactoring.

---
+++ TEF/src/editortest/mof/templates/MofClassTemplate.java
@@ -0,0 +1,45 @@
+package editortest.mof.template;
+
+import java.util.Arrays;
+import java.util.List;
+
+import editortest.model.IMetaModelElement;
+import editortest.model.IModelElement;
+import editortest.template.ElementTemplate;
+import editortest.template.IdentifierTemplate;
+import editortest.template.ListTemplate;
+import editortest.template.Template;
+import editortest.template.TerminalTemplate;
+import editortest.text.Proposal;
+import editortest.text.TextEvent;
+
+public class MofClassTemplate extends ElementTemplate {
+
+	public MofClassTemplate(IMetaModelElement metaModel) {
+		super(metaModel);
+	}
+
+	@Override
+	public Template[] createTemplates() {
+		return new Template[] { 
+				new TerminalTemplate("    class "),		
+				new IdentifierTemplate("name", getMetaModel()),
+				new TerminalTemplate(" {\n"),
+				new ListTemplate("ownedAttribute", getMetaModel()) {
+					@Override
+					public Template getElementTemplate() {
+						return new MofPropertyTemplate(getMetaModel().getType("Property"));
+					}	
+				},
+				new TerminalTemplate("    }\n")
+		};
+	}
+
+	@Override
+	public List<Proposal> getProposals() {
+		return Arrays.asList(new Proposal[] { 
+				new Proposal("class... ", "class <unnamed> {\n}\n")
+		});
+	}
+	
+}
---
+++ TEF/src/editortest/mof/templates/MofPropertyTemplate.java
@@ -0,0 +1,37 @@
+package editortest.mof.template;
+
+import java.util.Arrays;
+import java.util.List;
+
+import editortest.model.IMetaModelElement;
+import editortest.template.ElementTemplate;
+import editortest.template.IdentifierTemplate;
+import editortest.template.ReferenceTemplate;
+import editortest.template.Template;
+import editortest.template.TerminalTemplate;
+import editortest.text.Proposal;
+
+public class MofPropertyTemplate extends ElementTemplate {
+
+	public MofPropertyTemplate(IMetaModelElement metaModel) {
+		super(metaModel);
+	}
+
+	@Override
+	public Template[] createTemplates() {
+		return new Template[] {
+				new TerminalTemplate("        "),
+				new ReferenceTemplate("type", getMetaModel(), getMetaModel().getType("Type")),
+				new TerminalTemplate(" "),
+				new IdentifierTemplate("name", getMetaModel()),
+				new TerminalTemplate(";\n")
+		};
+	}
+	
+	@Override
+	public List<Proposal> getProposals() {
+		return Arrays.asList(new Proposal[] { 
+				new Proposal("property... ", "<borken-ref> <unnamed> {\n}\n")
+		});
+	}
+}

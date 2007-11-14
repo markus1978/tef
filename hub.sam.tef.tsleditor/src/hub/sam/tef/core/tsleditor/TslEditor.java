912976af36cef713fc45f79c12d13e14afde598a
Markus Scheidgen <do@not.use>
2007-11-14 17:02:19 +0000

    Initial import.

---
+++ hub.sam.tef.tsleditor/src/hub/sam/tef/core/tsleditor/TslEditor.java
@@ -0,0 +1,42 @@
+package hub.sam.tef.core.tsleditor;
+
+import hub.sam.tef.Utilities;
+import hub.sam.tef.editor.TextEditor;
+import hub.sam.tef.semantics.ISemanticsProvider;
+import hub.sam.tef.tsl.Syntax;
+import hub.sam.tef.tsl.TslPackage;
+import hub.sam.tef.tsl.provider.TslItemProviderAdapterFactory;
+import hub.sam.tef.tslsemantics.TslSemanticsProvider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
+
+public class TslEditor extends TextEditor {
+	@Override
+	public EPackage[] createMetaModelPackages() {
+		return new EPackage[] { TslPackage.eINSTANCE, EcorePackage.eINSTANCE };
+	}
+	
+	@Override
+	public String getPlatformURIOfSyntax() {
+		throw new RuntimeException("assert");
+	}	
+	
+	@Override
+	public AdapterFactory[] createItemProviderAdapterFactories() {
+		return new AdapterFactory[] { new TslItemProviderAdapterFactory(), 
+				new EcoreItemProviderAdapterFactory() };
+	}
+
+	@Override
+	public Syntax createSyntax() {
+		return Utilities.getTslSyntax();
+	}
+
+	@Override
+	public ISemanticsProvider createSemanticsProvider() {
+		return new TslSemanticsProvider();
+	}
+}
---
+++ hub.sam.tef.tsleditor/src/hub/sam/tef/tsleditor/TslEditorPlugin.java
@@ -0,0 +1,50 @@
+package hub.sam.tef.tsleditor;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class TslEditorPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "hub.sam.tef.tsleditor";
+
+	// The shared instance
+	private static TslEditorPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public TslEditorPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static TslEditorPlugin getDefault() {
+		return plugin;
+	}
+
+}

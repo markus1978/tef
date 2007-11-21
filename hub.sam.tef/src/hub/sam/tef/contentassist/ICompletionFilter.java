43c88dff1dc35fade569c974c5848ccabecbd2a5
Markus Scheidgen <do@not.use>
2007-11-21 18:50:31 +0000

    deleted.

--- hub.sam.tef/src/hub/sam/tef/contentassist/ICompletionFilter.java
+++
@@ -1,9 +0,0 @@
-package hub.sam.tef.contentassist;
-
-/**
- * An interface that can be used by completion collecting functions to allow the
- * user to filter unwanted completion proposals.
- */
-public interface ICompletionFilter {
-	public boolean accept(Object obj);
-}

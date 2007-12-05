43206067f30cb234ac552cae73018c8dd4a3ea54
Markus Scheidgen <do@not.use>
2007-12-05 15:35:08 +0000

    pop-up editors.

--- hub.sam.tef.tsleditor/src/hub/sam/tef/tsleditor/ExtendedTslEditor.java
+++ hub.sam.tef.tsleditor/src/hub/sam/tef/tsleditor/ExtendedTslEditor.java
@@ -1,10 +1,10 @@
 package hub.sam.tef.tsleditor;
 
 import hub.sam.tef.TEFPlugin;
-import hub.sam.tef.editor.TextEditor;
+import hub.sam.tef.editor.text.TextEditor;
 import hub.sam.tef.etsl.EtslPackage;
 import hub.sam.tef.etsl.provider.EtslItemProviderAdapterFactory;
-import hub.sam.tef.modelcreating.ModelCreatingContext;
+import hub.sam.tef.modelcreating.IModelCreatingContext;
 import hub.sam.tef.semantics.ISemanticsProvider;
 import hub.sam.tef.tsl.TslPackage;
 import hub.sam.tef.tsl.provider.TslItemProviderAdapterFactory;
@@ -15,6 +15,7 @@ import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.ui.part.FileEditorInput;
 import org.osgi.framework.Bundle;
 
@@ -47,9 +48,9 @@ public class ExtendedTslEditor extends TextEditor {
 	}
 	
 	@Override
-	public ModelCreatingContext createModelCreatingContext() {
+	public IModelCreatingContext createModelCreatingContext() {
 		return new TslModelCreatingContext(getMetaModelPackages(), 
-				getSemanticsProvider(), 
+				getSemanticsProvider(), new ResourceImpl(), getCurrentText(), 
 				((FileEditorInput)getEditorInput()).getFile().getProject());
 	}
 	
--- hub.sam.tef.tsleditor/src/hub/sam/tef/tsleditor/TslEditor.java
+++ hub.sam.tef.tsleditor/src/hub/sam/tef/tsleditor/TslEditor.java
@@ -2,8 +2,8 @@ package hub.sam.tef.tsleditor;
 
 import hub.sam.tef.TEFPlugin;
 import hub.sam.tef.Utilities;
-import hub.sam.tef.editor.TextEditor;
-import hub.sam.tef.modelcreating.ModelCreatingContext;
+import hub.sam.tef.editor.text.TextEditor;
+import hub.sam.tef.modelcreating.IModelCreatingContext;
 import hub.sam.tef.semantics.ISemanticsProvider;
 import hub.sam.tef.tsl.Syntax;
 import hub.sam.tef.tsl.TslPackage;
@@ -16,6 +16,7 @@ import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.ui.part.FileEditorInput;
 import org.osgi.framework.Bundle;
 
@@ -53,9 +54,9 @@ public class TslEditor extends TextEditor {
 	}
 
 	@Override
-	public ModelCreatingContext createModelCreatingContext() {		
+	public IModelCreatingContext createModelCreatingContext() {		
 		return new TslModelCreatingContext(getMetaModelPackages(), 
-				getSemanticsProvider(), 
+				getSemanticsProvider(), new ResourceImpl(), getCurrentText(),
 				((FileEditorInput)getEditorInput()).getFile().getProject());
 	}
 	
